e7ad9da33f93513424ed3862d727c66ad064ec36
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-01-06 04:41:32 +0300

    fix

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -242,6 +242,11 @@ class CarInterface(CarInterfaceBase):
     ret.cruiseState.available = bool(self.CS.main_on)
     ret.cruiseState.standstill = False
 
+    # Optima only has blinker flash signal
+    if self.CP.carFingerprint in [CAR.KIA_OPTIMA, CAR.KIA_OPTIMA_H]:
+      self.CS.left_blinker_on = self.CS.left_blinker_flash or self.CS.prev_left_blinker_on and self.CC.turning_signal_timer
+      self.CS.right_blinker_on = self.CS.right_blinker_flash or self.CS.prev_right_blinker_on and self.CC.turning_signal_timer
+
     # TODO: button presses
     buttonEvents = []
 
