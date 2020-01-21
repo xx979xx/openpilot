97191168ff9d7d1e7bc4c979ad4ae1c430eef59d
Arne Schwarck <arneschwarck@gmail.com>
2020-01-21 21:14:29 +0100

    Update ui.hpp

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -85,7 +85,9 @@ typedef struct UIScene {
   uint64_t v_cruise_update_ts;
   float v_ego;
   bool decel_for_model;
-
+  
+  float angleSteers;
+  
   float speedlimit;
   float speedlimitaheaddistance;
   bool speedlimitahead_valid;
