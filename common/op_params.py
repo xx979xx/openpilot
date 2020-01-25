d8e1ed188071f9d8488121df067073616c71187a
Shane Smiskol <shane@smiskol.com>
2020-01-24 18:48:58 -0600

    add opTune, better support for parameters without all the keys. (#522)
    * Better modularization for parameter's various keys. allowed types, description and live are no longer required for new parameters, but recommended. Fixed bug in delele_old

--- common/op_params.py
+++ common/op_params.py
@@ -26,6 +26,19 @@ def read_params(params_file, default_params):
 
 class opParams:
   def __init__(self):
+    """
+      To add your own parameter to opParams in your fork, simply add a new dictionary entry with the name of your parameter and its default value to save to new users' op_params.json file.
+      The description, allowed_types, and live keys are no longer required but recommended to help users edit their parameters with opEdit and opTune correctly.
+        - The description value will be shown to users when they use opEdit or opTune to change the value of the parameter.
+        - The allowed_types key is used to restrict what kinds of values can be entered with opEdit so that users can't reasonably break the fork with unintended behavior.
+          Limiting the range of floats or integers is still recommended when `.get`ting the parameter.
+          When a None value is allowed, use `type(None)` instead of None, as opEdit checks the type against the values in the key with `isinstance()`.
+        - Finally, the live key tells both opParams and opTune that it's a live parameter that will change. Therefore, you must place the `op_params.get()` call in the update function so that it can update.
+      Here's an example of the minimum required dictionary:
+
+      self.default_params = {'camera_offset': {'default': 0.06}}
+    """
+
     self.default_params = {'camera_offset': {'default': 0.06, 'allowed_types': [float, int], 'description': 'Your camera offset to use in lane_planner.py', 'live': True},
                            'awareness_factor': {'default': 10., 'allowed_types': [float, int], 'description': 'Multiplier for the awareness times', 'live': False},
                            'use_car_caching': {'default': True, 'allowed_types': [bool], 'description': 'Whether to use fingerprint caching', 'live': False},
@@ -41,6 +54,7 @@ class opParams:
     self.last_read_time = time.time()
     self.read_frequency = 5.0  # max frequency to read with self.get(...) (sec)
     self.force_update = False  # replaces values with default params if True, not just add add missing key/value pairs
+    self.to_delete = []
     self.run_init()  # restores, reads, and updates params
 
   def create_id(self):  # creates unique identifier to send with sentry errors. please update uniqueID with op_edit.py to your username!
@@ -78,6 +92,8 @@ class opParams:
       self.params, read_status = read_params(self.params_file, self.format_default_params())
       if read_status:
         to_write = not self.add_default_params()  # if new default data has been added
+        if self.delete_old():  # or if old params have been deleted
+          to_write = True
       else:  # don't overwrite corrupted params, just print to screen
         print("ERROR: Can't read op_params.json file")
     elif os.path.isfile(self.kegman_file):
@@ -93,22 +109,31 @@ class opParams:
     if to_write or no_params:
       write_params(self.params, self.params_file)
 
+  def delete_old(self):
+    prev_params = dict(self.params)
+    for i in self.to_delete:
+      if i in self.params:
+        del self.params[i]
+    return prev_params == self.params
+
   def put(self, key, value):
     self.params.update({key: value})
     write_params(self.params, self.params_file)
 
-  def get(self, key=None, default=None):  # can specify a default value if key doesn't exist
+  def get(self, key=None, default=None, force_update=False):  # can specify a default value if key doesn't exist
+    self.update_params(key, force_update)
     if key is None:
       return self.params
-    if not travis and key in self.default_params and self.default_params[key]['live']:  # if is a live param, we want get updates while openpilot is running
-      if time.time() - self.last_read_time >= self.read_frequency:  # make sure we aren't reading file too often
+    return self.params[key] if key in self.params else default
+
+  def update_params(self, key, force_update):
+    if force_update or (key in self.default_params and 'live' in self.default_params[key] and self.default_params[key]['live']):  # if is a live param, we want to get updates while openpilot is running
+      if not travis and time.time() - self.last_read_time >= self.read_frequency:  # make sure we aren't reading file too often
         self.params, read_status = read_params(self.params_file, self.format_default_params())
         if not read_status:
-          time.sleep(0.01)
-          self.params, read_status = read_params(self.params_file, self.format_default_params())  # if the file was being written to, retry once
+          time.sleep(1/100.)
+          self.params, _ = read_params(self.params_file, self.format_default_params())  # if the file was being written to, retry once
         self.last_read_time = time.time()
-
-    return self.params[key] if key in self.params else default
 
   def delete(self, key):
     if key in self.params:
