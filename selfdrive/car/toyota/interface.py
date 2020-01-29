2b0f6841b64606a6f1c92e75b0289d595eeca272
Arne Schwarck <arneschwarck@gmail.com>
2020-01-29 20:58:43 +0100

    increase stop pid longitudinalTuning factor at 0

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -273,12 +273,12 @@ class CarInterface(CarInterfaceBase):
     if ret.enableGasInterceptor:
       ret.gasMaxBP = [0., 9., 55]
       ret.gasMaxV = [0.2, 0.5, 0.7]
-      ret.longitudinalTuning.kpV = [0.50, 0.4, 0.3]  # braking tune
+      ret.longitudinalTuning.kpV = [1.0, 0.4, 0.3]  # braking tune
       ret.longitudinalTuning.kiV = [0.135, 0.1]
     else:
       ret.gasMaxBP = [0., 9., 55]
       ret.gasMaxV = [0.2, 0.5, 0.7]
-      ret.longitudinalTuning.kpV = [0.325, 0.325, 0.325]  # braking tune from rav4h
+      ret.longitudinalTuning.kpV = [1.5, 0.325, 0.325]  # braking tune from rav4h
       ret.longitudinalTuning.kiV = [0.1, 0.10]
 
     return ret
