610b99f4ddfb208e27371f7a2be38f9cfd9bc6f8
Arne Schwarck <arneschwarck@gmail.com>
2020-01-29 18:01:56 +0100

    return longcontrol to stock

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -72,7 +72,7 @@ class LongControl():
     self.pid.reset()
     self.v_pid = v_pid
 
-  def update(self, active, v_ego, gas_pressed, brake_pressed, standstill, cruise_standstill, v_cruise, v_target, v_target_future, a_target, CP, gas_button_status, has_lead):
+  def update(self, active, v_ego, gas_pressed, brake_pressed, standstill, cruise_standstill, v_cruise, v_target, v_target_future, a_target, CP):#, gas_button_status, has_lead):
     """Update longitudinal control. This updates the state machine and runs a PID loop"""
     # Actuation limits
     #enableGasInterceptor = CP.enableGasInterceptor
