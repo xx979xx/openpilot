68a6aa8a6c930b6e1038f8c766882af5a1b4d141
Kyle Greenlaw <Kyroapps@gmail.com>
2020-01-31 07:27:27 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -75,7 +75,7 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 1344. + STD_CARGO_KG  # Spec
       ret.wheelbase = 2.65  # Spec
       ret.steerRatio = 13.4  # Spec
-      tire_stiffness_factor = 0.385
+      tire_stiffness_factor = 0.5
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
       ret.minSteerSpeed = 32 * CV.MPH_TO_MS
