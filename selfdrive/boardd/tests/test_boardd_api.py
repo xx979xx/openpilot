a2dbcef4a3e76878efafdcad6a9336e9a28cf0ab
Arne Schwarck <arneschwarck@gmail.com>
2020-02-07 21:23:48 +0100

    rather use str.encode

--- selfdrive/boardd/tests/test_boardd_api.py
+++ selfdrive/boardd/tests/test_boardd_api.py
@@ -13,8 +13,8 @@ def generate_random_can_data_list():
   can_list = []
   cnt = random.randint(1, 64)
   for j in range(cnt):
-    can_data = bytes(np.random.bytes(random.randint(1, 8)))
-    can_list.append([random.randint(0, 128), random.randint(0, 128), can_data, random.randint(0, 128)])
+    can_data = np.random.bytes(random.randint(1, 8))
+    can_list.append([random.randint(0, 128), random.randint(0, 128), str.encode(can_data), random.randint(0, 128)])
   return can_list, cnt
 
 
