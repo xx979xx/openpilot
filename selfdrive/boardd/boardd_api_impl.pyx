be704b34166ede643aed3bd0aef99f5bb0db6eda
Arne Schwarck <arneschwarck@gmail.com>
2020-02-08 18:48:38 +0100

    Update boardd_api_impl.pyx

--- selfdrive/boardd/boardd_api_impl.pyx
+++ selfdrive/boardd/boardd_api_impl.pyx
@@ -18,14 +18,15 @@ def can_list_to_can_capnp(can_msgs, msgtype='can', valid=True):
   for can_msg in can_msgs:
     f.address = can_msg[0]
     f.busTime = can_msg[1]
-    try:
-      f.dat = can_msg[2].hex()
-    except:
-      f.dat = can_msg[2]
-      print("address")
-      print(f.address)
-      print("dat")
-      print(f.dat)
+    f.dat = can_msg[2]
+    #try:
+      #f.dat = can_msg[2].hex()
+    #except:
+      #f.dat = can_msg[2]
+      #print("address")
+      #print(f.address)
+      #print("dat")
+      #print(f.dat)
     if f.address == 0x750:
       print(f.dat)
     f.src = can_msg[3]
