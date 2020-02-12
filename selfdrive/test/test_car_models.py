db3158f9eb73f69b7d0604810d63ea364072efe7
Arne Schwarck <arneschwarck@gmail.com>
2020-02-12 09:35:03 +0100

    also exclude CHRYSLER.JEEP_CHEROKEE_2020

--- selfdrive/test/test_car_models.py
+++ selfdrive/test/test_car_models.py
@@ -71,10 +71,10 @@ routes = {
     'carFingerprint': CHRYSLER.PACIFICA_2019_HYBRID,
     'enableCamera': True,
   },
-  "8190c7275a24557b|2020-01-29--08-33-58": {
-    'carFingerprint': CHRYSLER.PACIFICA_2020_HYBRID,
-    'enableCamera': True,
-  },
+  #"8190c7275a24557b|2020-01-29--08-33-58": {
+  #  'carFingerprint': CHRYSLER.PACIFICA_2020_HYBRID,
+  #  'enableCamera': True,
+  #},
   # This pacifica was removed because the fingerprint seemed from a Volt
   #"9f7a7e50a51fb9db|2019-01-03--14-05-01": {
   #  'carFingerprint': CHRYSLER.PACIFICA_2018,
@@ -470,7 +470,7 @@ non_public_routes = [
 ]
 
 # TODO: add routes for these cars
-non_tested_cars = [TOYOTA.LEXUS_CTH,TOYOTA.LEXUS_IS,TOYOTA.LEXUS_ISH, CHRYSLER.PACIFICA_2018, HONDA.ACCORDH, CHRYSLER.PACIFICA_2017, CHRYSLER.JEEP_CHEROKEE_2017, CHRYSLER.JEEP_CHEROKEE_2018, CHRYSLER.JEEP_CHEROKEE_2019]
+non_tested_cars = [TOYOTA.LEXUS_CTH,TOYOTA.LEXUS_IS,TOYOTA.LEXUS_ISH, CHRYSLER.PACIFICA_2018, HONDA.ACCORDH, CHRYSLER.PACIFICA_2017, CHRYSLER.JEEP_CHEROKEE_2017, CHRYSLER.JEEP_CHEROKEE_2018, CHRYSLER.JEEP_CHEROKEE_2019, CHRYSLER.JEEP_CHEROKEE_2020]
 
 if __name__ == "__main__":
 
