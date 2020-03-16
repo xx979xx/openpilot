b6e86f6e2ba0c81c21299e124d11bd9af04317c2
Oscar Moreno <60990482+moreno7798@users.noreply.github.com>
2020-03-16 18:07:19 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -148,10 +148,10 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 3305. * CV.LB_TO_KG + STD_CARGO_KG
       ret.wheelbase = 2.66
-      ret.steerRatio = 14.0 * 1.1   # 10% higher at the center seems reasonable
+      ret.steerRatio = 15.0 * 1.1   # 10% higher at the center seems reasonable
       tire_stiffness_factor = 0.5
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
+      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[9., 22.], [9., 22.]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2, 0.35], [0.05, 0.09]]
       ret.minSteerSpeed = 0.
 
     ret.minEnableSpeed = -1.   # enable is done by stock ACC, so ignore this
