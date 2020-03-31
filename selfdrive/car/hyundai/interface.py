527927a761d7135bd8594b2d2f0e335460c95a0f
Oscar Moreno <60990482+moreno7798@users.noreply.github.com>
2020-03-31 00:13:36 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -147,11 +147,12 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.KIA_SPORTAGE:
       ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 3305. * CV.LB_TO_KG + STD_CARGO_KG
-      ret.wheelbase = 2.7
-      ret.steerRatio = 11.0
+      ret.wheelbase = 2.66
+      ret.steerRatio = 10.1
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
       ret.minSteerSpeed = 0.
+      tire_stiffness_factor = 0.5
 
     ret.minEnableSpeed = -1.   # enable is done by stock ACC, so ignore this
 
