ae4ef4a9ef8c97564a86330e4638a261f8d66b37
Oscar Moreno <60990482+moreno7798@users.noreply.github.com>
2020-05-27 00:56:26 -0400

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -14,7 +14,6 @@ class SteerLimitParams:
 
 class CAR:
   ELANTRA = "HYUNDAI ELANTRA LIMITED ULTIMATE 2017"
-  GENESIS = "HYUNDAI GENESIS 2018"
   ELANTRA_GT_I30 = "HYUNDAI I30 N LINE 2019 & GT 2018 DCT"
   GENESIS_G80 = "GENESIS G80 2017"
   GENESIS_G90 = "GENESIS G90 2017"
@@ -32,7 +31,8 @@ class CAR:
   KIA_SPORTAGE = "KIA SPORTAGE S 2020"
   SONATA = "HYUNDAI SONATA 2020"
   PALISADE = "HYUNDAI PALISADE 2020"
-
+  SONATA_H = "HYUNDAI SONATA HYBRID 2020"
+  
 
 class Buttons:
   NONE = 0
@@ -147,6 +147,10 @@ FINGERPRINTS = {
   CAR.PALISADE: [{
     67: 8, 127: 8, 304: 8, 320: 8, 339: 8, 356: 4, 544: 8, 549: 8, 576: 8, 593: 8, 608: 8, 688: 6, 809: 8, 832: 8, 854: 7, 870: 7, 871: 8, 872: 8, 897: 8, 902: 8, 903: 8, 905: 8, 909: 8, 913: 8, 916: 8, 1040: 8, 1042: 8, 1056: 8, 1057: 8, 1064: 8, 1078: 4, 1107: 5, 1123: 8, 1136: 8, 1151: 6, 1155: 8, 1156: 8, 1157: 4, 1162: 8, 1164: 8, 1168: 7, 1170: 8, 1173: 8, 1180: 8, 1186: 2, 1191: 2, 1193: 8, 1210: 8, 1225: 8, 1227: 8, 1265: 4, 1280: 8, 1287: 4, 1290: 8, 1292: 8, 1294: 8, 1312: 8, 1322: 8, 1342: 6, 1345: 8, 1348: 8, 1363: 8, 1369: 8, 1371: 8, 1378: 8, 1384: 8, 1407: 8, 1419: 8, 1427: 6, 1456: 4, 1470: 8, 2000: 8, 2005: 8, 2008: 8
   }],
+  CAR.SONATA_H: [{
+    127: 8, 304: 8, 320: 8, 339: 8, 352: 8, 356: 4, 544: 8, 546: 8, 548: 8, 576: 8, 593: 8, 688: 6, 757: 2, 832: 8, 865: 8, 881: 8, 882: 8, 897: 8, 902: 8, 903: 8, 905: 8, 909: 8, 913: 8, 916: 8, 1040: 8, 1042: 8, 1056: 8, 1057: 8, 1078: 4, 1102: 8, 1108: 8, 1114: 8, 1136: 6, 1138: 5, 1151: 8, 1155: 8, 1156: 8, 1157: 4, 1162: 8, 1164: 8, 1168: 8, 1173: 8, 1180: 8, 1184: 8, 1186: 2, 1191: 2, 1193: 8, 1210: 8, 1225: 8, 1227: 8, 1265: 4, 1268: 8, 1280: 8, 1287: 4, 1290: 8, 1291: 8, 1292: 8, 1294: 8, 1312: 8, 1322: 8, 1330: 8, 1339: 8, 1342: 6, 1343: 8, 1345: 8, 1348: 8, 1355: 8, 1363: 8, 1369: 8, 1371: 8, 1378: 8, 1407: 8, 1419: 8, 1427: 6, 1429: 8, 1430: 8, 1446: 8, 1448: 8, 1456: 4, 1460: 8, 1470: 8, 1476: 8, 1535: 8
+  }],
+
 }
 
 ECU_FINGERPRINT = {
@@ -154,20 +158,19 @@ ECU_FINGERPRINT = {
 }
 
 CHECKSUM = {
-  "crc8": [CAR.SANTA_FE, CAR.SANTA_FE_1, CAR.SONATA, CAR.PALISADE],
+  "crc8": [CAR.SANTA_FE, CAR.SANTA_FE_1, CAR.SONATA, CAR.PALISADE, CAR.SONATA_H],
   "6B": [CAR.KIA_SORENTO, CAR.HYUNDAI_GENESIS],
 }
 
 FEATURES = {
   "use_cluster_gears": [CAR.ELANTRA, CAR.KONA, CAR.ELANTRA_GT_I30],     # Use Cluster for Gear Selection, rather than Transmission
-  "use_tcu_gears": [],
-  "use_elect_gears": [CAR.KIA_OPTIMA_H, CAR.IONIQ_EV_LTD, CAR.KONA_EV], # Use TCU Message for Gear Selection
-  "use_elect_ems": [],                                        # Use E_EMS11 Message for Gas and Brake for Hybrid/ELectric
+  "use_tcu_gears": [CAR.KIA_SPORTAGE],
+  "use_elect_gears": [CAR.KIA_OPTIMA_H, CAR.IONIQ_EV_LTD, CAR.KONA_EV, CAR.SONATA_H], # Use TCU Message for Gear Selection
+  "use_elect_ems": [CAR.SONATA_H],                                        # Use E_EMS11 Message for Gas and Brake for Hybrid/ELectric
 }
 
 DBC = {
   CAR.ELANTRA: dbc_dict('hyundai_kia_generic', None),
-  CAR.GENESIS: dbc_dict('hyundai_kia_generic', None),
   CAR.ELANTRA_GT_I30: dbc_dict('hyundai_kia_generic', None),
   CAR.GENESIS_G80: dbc_dict('hyundai_kia_generic', None),
   CAR.GENESIS_G90: dbc_dict('hyundai_kia_generic', None),
@@ -185,6 +188,7 @@ DBC = {
   CAR.KIA_SPORTAGE: dbc_dict('hyundai_kia_generic', None),
   CAR.SONATA: dbc_dict('hyundai_kia_generic', None),
   CAR.PALISADE: dbc_dict('hyundai_kia_generic', None),
+  CAR.SONATA_H: dbc_dict('hyundai_kia_generic', None),
 }
 
 STEER_THRESHOLD = 150
