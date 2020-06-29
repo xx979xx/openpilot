ab449cfb325f32f38e1919b551a194223b2cad50
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-29 17:10:45 -0400

    update June29

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -188,10 +188,10 @@ def calibration_incomplete_alert(CP, sm, metric):
     Priority.LOWEST, VisualAlert.none, AudibleAlert.none, 0., 0., .2)
 
 def no_gps_alert(CP, sm, metric):
-  two = sm['health'].hwType == log.HealthData.HwType.uno
+  gps_integrated = sm['health'].hwType in [log.HealthData.HwType.uno, log.HealthData.HwType.dos]
   return Alert(
     "Poor GPS reception",
-    "If sky is visible, contact support" if two else "Check GPS antenna placement",
+    "If sky is visible, contact support" if gps_integrated else "Check GPS antenna placement",
     AlertStatus.normal, AlertSize.mid,
     Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., .2, creation_delay=300.)
 
@@ -203,8 +203,6 @@ def wrong_car_mode_alert(CP, sm, metric):
 
 EVENTS = {
   # ********** events with no alerts **********
-
-  #EventName.gasPressed: {ET.PRE_ENABLE: None},
 
   # ********** events only containing alerts displayed in all states **********
 
@@ -332,6 +330,14 @@ EVENTS = {
   },
 
   # ********** events only containing alerts that display while engaged **********
+
+  EventName.gasPressed: {
+    ET.PRE_ENABLE: Alert(
+      "openpilot will not brake while gas pressed",
+      "",
+      AlertStatus.normal, AlertSize.small,
+      Priority.LOWEST, VisualAlert.none, AudibleAlert.none, .0, .0, .1),
+  },
 
   EventName.vehicleModelInvalid: {
     ET.WARNING: Alert(
